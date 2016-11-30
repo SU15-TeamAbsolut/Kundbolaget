123234874d8ba8ad449ea9c3389326e112a2dfee
Johan Svensson <johan@atomicplayboy.net>
2016-11-30 15:34:01 +0100

    #2 Supplier seed data, controller complete
    Supplier has IsActive flag

--- Kundbolaget/Migrations/Configuration.cs
+++ Kundbolaget/Migrations/Configuration.cs
@@ -18,7 +18,11 @@ namespace Kundbolaget.Migrations
         {
             Supplier[] suppliers =
             {
-                new Supplier { Name = "Bengts Vodka", Email = "bengt@example.net" }
+                new Supplier { Id = 1, Name = "Bengts Bärs", Email = "bengt@example.net" },
+                new Supplier { Id = 2, Name = "Vlads Vodka", Email = "vlad@example.net" },
+                new Supplier { Id = 3, Name = "Cissis Cider", Email = "cissi@example.net" },
+                new Supplier { Id = 4, Name = "Pontus Punsch", Email = "pontus@example.net" },
+                new Supplier { Id = 5, Name = "Håkans Hembränt", Email = "håkan@example.net" },
             };
 
             context.Suppliers.AddOrUpdate(suppliers);
--- Kundbolaget/Models/EntityModels/Supplier.cs
+++ Kundbolaget/Models/EntityModels/Supplier.cs
@@ -9,5 +9,6 @@ namespace Kundbolaget.Models.EntityModels
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public bool IsActive { get; set; }
     }
 }
