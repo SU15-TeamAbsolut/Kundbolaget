7bb9b23d3fb5dee4ccf13c17921c95bf71423f07
Johan Svensson <johan@atomicplayboy.net>
2016-11-30 14:06:19 +0100

    #2 Added initial database

---
+++ Kundbolaget/EntityFramework/Contexts/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.EntityFramework.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
