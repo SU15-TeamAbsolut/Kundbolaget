db620e49da800266ff1b3e614fde91259d5dff54
Christofer Sundell <coffasund@gmail.com>
2016-12-02 13:11:04 +0100

    Created AlcoholLicense model

---
+++ Kundbolaget/Models/EntityModels/AlcoholLicense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Kundbolaget.Models.EntityModels
+{
+    public class AlcoholLicense
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public bool IsValid { get; set; }
+        public virtual Customer Customers { get; set; }
+        public virtual Address ShippingAddresses { get; set; }
+
+    }
+}
