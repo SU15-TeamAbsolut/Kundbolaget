1cfea8d0f57589be81f9daf92a862c0442a89620
Johan Svensson <johan@atomicplayboy.net>
2016-12-05 11:39:58 +0100

    Fix migrations after merge

--- Kundbolaget/Migrations/Configuration.cs
+++ Kundbolaget/Migrations/Configuration.cs
@@ -9,29 +9,32 @@ namespace Kundbolaget.Migrations
     using Kundbolaget.Models.EntityModels;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Kundbolaget.EntityFramework.Contexts.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Kundbolaget.EntityFramework.Contexts.DataContext context)
+        protected override void Seed(DataContext context)
         {
+            SeedSuppliers(context);
             SeedAlcoholLicenses(context);
+            SeedCustomers(context);
+        }
 
+        private void SeedSuppliers(DataContext context)
+        {
             Supplier[] suppliers =
             {
-                new Supplier { Id = 1, Name = "Bengts Bärs", Email = "bengt@example.net" },
-                new Supplier { Id = 2, Name = "Vlads Vodka", Email = "vlad@example.net" },
-                new Supplier { Id = 3, Name = "Cissis Cider", Email = "cissi@example.net" },
-                new Supplier { Id = 4, Name = "Pontus Punsch", Email = "pontus@example.net" },
-                new Supplier { Id = 5, Name = "Håkans Hembränt", Email = "håkan@example.net" },
+                new Supplier {Id = 1, Name = "Bengts Bärs", Email = "bengt@example.net"},
+                new Supplier {Id = 2, Name = "Vlads Vodka", Email = "vlad@example.net"},
+                new Supplier {Id = 3, Name = "Cissis Cider", Email = "cissi@example.net"},
+                new Supplier {Id = 4, Name = "Pontus Punsch", Email = "pontus@example.net"},
+                new Supplier {Id = 5, Name = "Håkans Hembränt", Email = "håkan@example.net"},
             };
 
             context.Suppliers.AddOrUpdate(suppliers);
-
-            SeedCustomers(context);
         }
 
         private void SeedCustomers(DataContext context)
@@ -62,9 +65,9 @@ namespace Kundbolaget.Migrations
         {
             AlcoholLicense[] alcoholLicenses =
             {
-                new AlcoholLicense {StartDate = DateTime.Today, EndDate = new DateTime(2020,12,24), IsValid = true},
-                new AlcoholLicense {StartDate = DateTime.Today, EndDate = new DateTime(2021,12,24), IsValid = true},
-                new AlcoholLicense {StartDate = DateTime.Today, EndDate = new DateTime(2022,12,24), IsValid = true}
+                new AlcoholLicense {Id = 1, StartDate = DateTime.Today, EndDate = new DateTime(2020,12,24), IsValid = true},
+                new AlcoholLicense {Id = 2, StartDate = DateTime.Today, EndDate = new DateTime(2021,12,24), IsValid = true},
+                new AlcoholLicense {Id = 3, StartDate = DateTime.Today, EndDate = new DateTime(2022,12,24), IsValid = true}
             };
 
             context.AlcoholLicenses.AddOrUpdate(alcoholLicenses);
