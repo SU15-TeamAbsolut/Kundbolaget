160e1eccc157cb6110b7be8de8e352d50019bc7d
Niklas <nevland1@gmail.com>
2016-12-05 11:58:19 +0100

    # 4 Articles and Categories

---
+++ Kundbolaget/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kundbolaget.EntityFramework.Repositories;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.Controllers
+{
+    public class CategoryController : Controller
+    {
+     
+        private readonly IRepository<Category> _categoryRepository;
+        // GET: Product
+        public CategoryController()
+        {
+            _categoryRepository = new DataRepository<Category>();
+        }
+
+        public ActionResult Index()
+        {
+            var model = _categoryRepository.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _categoryRepository.Create(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = _categoryRepository.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _categoryRepository.Update(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = _categoryRepository.Find(id);
+            return View(model);
+        }
+    }
+}
--- Kundbolaget/EntityFramework/Contexts/DataContext.cs
+++ Kundbolaget/EntityFramework/Contexts/DataContext.cs
@@ -6,5 +6,7 @@ namespace Kundbolaget.EntityFramework.Contexts
     public class DataContext : DbContext
     {
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
