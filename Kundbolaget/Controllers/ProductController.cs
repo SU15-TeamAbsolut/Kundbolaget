cad0241b4cf1bef4c1d4150a2631ce658ad902b7
Niklas <nevland1@gmail.com>
2016-12-06 20:35:02 +0100

    13# Show products for specific category

--- Kundbolaget/Controllers/ProductController.cs
+++ Kundbolaget/Controllers/ProductController.cs
@@ -5,23 +5,58 @@ using System.Web;
 using System.Web.Mvc;
 using Kundbolaget.EntityFramework.Repositories;
 using Kundbolaget.Models.EntityModels;
+using Kundbolaget.ViewModels;
 
 namespace Kundbolaget.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IRepository<ProductCategory> _productCategoryRepository;
         private readonly IRepository<Product> _productRepository;
         // GET: Product
         public ProductController()
         {
             _productRepository = new DataRepository<Product>();
+            _productCategoryRepository = new DataRepository<ProductCategory>();
         }
 
         public ActionResult Index()
         {
-            var model = _productRepository.GetAll();
 
-            return View(model);
+            var viewModel = new ProductCategoryViewModel()
+            {
+                Products = _productRepository.GetAll(),
+                ProductCategories = _productCategoryRepository.GetAll(),
+                ProductCategory = new ProductCategory()
+            };
+            
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            int id = 0;
+            int.TryParse(Request["ProductCategory"], out id);
+            List<Product> productList;
+
+            if (id == 0)
+            {
+                productList = _productRepository.GetAll().ToList();
+            }
+            else
+            {
+                productList = _productRepository.GetAll()
+                   .Where(x => x.ProductCategoryId == id).ToList();
+            }
+                
+            var viewModel = new ProductCategoryViewModel()
+            {
+                Products = productList,
+                ProductCategories = _productCategoryRepository.GetAll(),
+                ProductCategory = new ProductCategory()
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult Create()
