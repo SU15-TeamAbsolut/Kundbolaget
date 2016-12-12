cae6addda7ccc1019ad3d35583916034fe2ba7ce
Phoenix5793 <andreascarlen95@hotmail.com>
2016-12-12 09:40:03 +0100

    slight change to FK in Address model

--- Kundbolaget/Models/EntityModels/Address.cs
+++ Kundbolaget/Models/EntityModels/Address.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -17,6 +18,7 @@ namespace Kundbolaget.Models.EntityModels
         public string City { get; set; }
         [Required]
         public int CountryId { get; set; }
+        [ForeignKey("CountryId")]
         public virtual Country Country { get; set; }
     }
 }
