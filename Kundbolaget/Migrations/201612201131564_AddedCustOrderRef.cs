a92577e5dc67a2feaff709982bb59a9357d86b0f
Christofer Sundell <coffasund@gmail.com>
2016-12-20 13:04:16 +0100

    #99 Added new migration for CustOrderRef

---
+++ Kundbolaget/Migrations/201612201131564_AddedCustOrderRef.cs
@@ -0,0 +1,18 @@
+namespace Kundbolaget.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCustOrderRef : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "CustomerOrderRef", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "CustomerOrderRef");
+        }
+    }
+}
