7c9ecdbbdd8d7afc8bd881e5c9c0db09f6decec9
Johan Svensson <johan@atomicplayboy.net>
2016-12-23 16:23:00 +0100

    #125 Include products in order rows from order repo
    Add `AmountInStock` to OrderRow model, not mapped to DB

--- Kundbolaget/Models/EntityModels/OrderRow.cs
+++ Kundbolaget/Models/EntityModels/OrderRow.cs
@@ -13,14 +13,15 @@ namespace Kundbolaget.Models.EntityModels
         [ForeignKey("OrderId")]
         public virtual Order Order { get; set; }
 
-        public int AmountOrdered { get; set; }
         [Required]
-
         public int ProductId { get; set; }
         [ForeignKey("ProductId")]
-
         public virtual Product Product { get; set; }
         public decimal Price {get; set;}
+
+        public int AmountOrdered { get; set; }
         public int? AmountShipped { get; set; }
+        [NotMapped]  // Used for order rows when fulfilling an order
+        public int AmountInStock { get; set; }
     }
 }
