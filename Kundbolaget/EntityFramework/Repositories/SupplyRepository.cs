5a58e4964d9bad23f143ae312c0539fa8b1bc432
Johan Svensson <johan@atomicplayboy.net>
2016-12-26 20:08:20 +0100

    #125 Add supply repository

---
+++ Kundbolaget/EntityFramework/Repositories/SupplyRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Kundbolaget.EntityFramework.Contexts;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.EntityFramework.Repositories
+{
+    public class SupplyRepository : DataRepository<ProductShelf>
+    {
+        public ProductShelf FindByProduct(int productId)
+        {
+            using (var db = new DataContext())
+            {
+                return db.ProductsShelves
+                    .Include(e => e.Product)
+                    .SingleOrDefault();
+            }
+        }
+
+        public ProductShelf FindByProduct(Product product)
+        {
+            return FindByProduct(product.Id);
+        }
+
+        public int GetAmountInStock(int productId)
+        {
+            using (var db = new DataContext())
+            {
+                return db.ProductsShelves
+                    .Where(r => r.ProductId == productId)
+                    .Select(r => r.CurrentAmount)
+                    .SingleOrDefault();
+            }
+        }
+    }
+}
