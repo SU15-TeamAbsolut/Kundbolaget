b1154dda972bc45af6ae796c81bc24314899c4ea
Johan Svensson <johan@atomicplayboy.net>
2016-12-28 16:32:04 +0100

    Update-Database now prints validation errors in console

--- Kundbolaget/EntityFramework/Contexts/DataContext.cs
+++ Kundbolaget/EntityFramework/Contexts/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
+using System.Text;
 using Kundbolaget.Models.EntityModels;
 
 namespace Kundbolaget.EntityFramework.Contexts
@@ -25,6 +27,33 @@ namespace Kundbolaget.EntityFramework.Contexts
         {
             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
             modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
         }
     }
 }
