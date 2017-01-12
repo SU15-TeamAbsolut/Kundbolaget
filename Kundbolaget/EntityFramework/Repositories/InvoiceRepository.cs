ecc03b7227475fb24b0d837fd5643e749719dd79
Johan Svensson <johan@atomicplayboy.net>
2017-01-12 13:21:55 +0100

    #74 Invoice status, repository, navbar menu
     - View for selecting customer to invoice

---
+++ Kundbolaget/EntityFramework/Repositories/InvoiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.EntityFramework.Repositories
+{
+    public class InvoiceRepository : DataRepository<Invoice>
+    {
+    }
+}
---
+++ Kundbolaget/Models/ViewModels/Invoice/InvoicableCustomerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kundbolaget.Enums;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.Models.ViewModels.Invoice
+{
+    public class InvoicableCustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public int InvoicableOrders { get; set; }
+
+        public static InvoicableCustomerViewModel FromCustomer(Customer customer)
+        {
+            return new InvoicableCustomerViewModel
+            {
+                Customer = customer,
+                InvoicableOrders = customer.Orders
+                    .Count(o => o.OrderStatus == OrderStatus.Delivered)
+            };
+        }
+    }
+}
